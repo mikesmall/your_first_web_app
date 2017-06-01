5d32c70fd35045950c400f04684d7047a9af3a6d
mikesmall <michael.l.a.small@gmail.com>
2017-06-01 14:33:17 -0400

    Made the 'Favourites' route work on my first try, but discovered an error in the provided 'Portfolio/Gallery' code...

--- server.rb
+++ server.rb
@@ -3,14 +3,24 @@ require 'sinatra'
   # instructions for what to do when user visits '/home' will go here
   get '/home' do
     erb :index
-  end#get
+  end#home
 
   get '/portfolio' do
     erb :gallery
-  end
+  end#portfolio
 
   get '/about_me' do
-  @skills = ['git', 'HTML', 'CSS', 'Ruby']
-  @interests = ['cats', 'art', 'music', 'films', 'coffee']
-  erb :about_me
-end
+    @skills = ['git', 'HTML', 'CSS', 'Ruby']
+    @interests = ['cats', 'art', 'music', 'films', 'coffee']
+    erb :about_me
+  end#about_me
+
+  get '/favourites' do
+    @fav_links = [
+      'http://warnerbros.com/archive/spacejam/movie/jam.htm',
+      'http://instantrimshot.com',
+      'http://nooooooooooooooo.com',
+      'http://torontoist.com'
+    ]
+    erb :favourites 
+  end#favourites
