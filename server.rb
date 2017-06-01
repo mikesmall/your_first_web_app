278b7eff9544fca3b278fb00da520c6c0dc4f9b5
mikesmall <michael.l.a.small@gmail.com>
2017-06-01 14:20:54 -0400

    'Incorporating data in a view' step complete

--- server.rb
+++ server.rb
@@ -6,5 +6,11 @@ require 'sinatra'
   end#get
 
   get '/portfolio' do
-    erb :gallery 
+    erb :gallery
   end
+
+  get '/about_me' do
+  @skills = ['git', 'HTML', 'CSS', 'Ruby']
+  @interests = ['cats', 'art', 'music', 'films', 'coffee']
+  erb :about_me
+end
